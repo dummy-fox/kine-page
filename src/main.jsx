a8cdb51b53ead0ff7e0a38dd554352d123695e1c
dummy-fox <jarenrc7654@gmail.com>
2025-05-31 22:37:15 -0500

    removing references of react-helmet-async

--- src/main.jsx
+++ src/main.jsx
@@ -3,13 +3,11 @@ import ReactDOM from 'react-dom/client';
 import App from './App';
 import './index.css';
 import { BrowserRouter, Routes, Route } from 'react-router-dom';
-import { HelmetProvider } from 'react-helmet-async';
 import NotFound from './components/NotFound';
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
     
-    <HelmetProvider>
       <BrowserRouter>
         <Routes>
           <Route path='/' element={<NotFound/>}/>
@@ -17,7 +15,6 @@ ReactDOM.createRoot(document.getElementById('root')).render(
           <Route path='*' element={<NotFound/>}/>
         </Routes>
       </BrowserRouter>
-    </HelmetProvider>
 
   </React.StrictMode>
 );
