6dbe7088580a7aa7844f02e5337b70287011b747
dummy-fox <jarenrc7654@gmail.com>
2025-06-04 15:53:03 -0500

    adding metatags

--- vite.config.js
+++ vite.config.js
@@ -1,18 +1,22 @@
+// vite.config.js
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 import path from 'path'
 import { fileURLToPath } from 'url'
 
-// Estas dos líneas son el reemplazo de __dirname en módulos ES
 const __filename = fileURLToPath(import.meta.url)
 const __dirname = path.dirname(__filename)
 
 export default defineConfig({
   plugins: [react()],
-  base: '/', 
+  base: '/',
   resolve: {
     alias: {
       '@': path.resolve(__dirname, './src'),
     },
+  },
+  build: {
+    outDir: 'build',    // ← así Vite genera en "build/" en vez de "dist/"
+    target: 'es2015'    // ← importante para que React Snap no rompa con código moderno
   },
 })
